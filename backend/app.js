9a2671821639f8d19907de5a0b2553f01b5dfbe2
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2024-02-23 12:49:01 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin: "localhost:3000", credentials: true }))
+app.use(cors({ origin: "http://localhost:3000", credentials: true }))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/invite', inviteRouter);
